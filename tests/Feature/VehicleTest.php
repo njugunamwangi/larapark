e9b6fc3dff5a754697a9a1203614a987dd4fc641
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-10 01:03:05 +0300

    Add tests to vehicle

--- tests/Feature/VehicleTest.php
+++ tests/Feature/VehicleTest.php
@@ -32,4 +32,58 @@ class VehicleTest extends TestCase
             ->assertJsonPath('data.0.plate_number', $vehicleForJohn->plate_number)
             ->assertJsonMissing($vehicleForAdam->toArray());
     }
+
+    public function testUserCanCreateVehicle()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->postJson('/api/v1/vehicles', [
+            'plate_number' => 'AAA111',
+        ]);
+
+        $response->assertStatus(201)
+            ->assertJsonStructure(['data'])
+            ->assertJsonCount(2, 'data')
+            ->assertJsonStructure([
+                'data' => ['0' => 'plate_number'],
+            ])
+            ->assertJsonPath('data.plate_number', 'AAA111');
+
+        $this->assertDatabaseHas('vehicles', [
+            'plate_number' => 'AAA111',
+        ]);
+    }
+
+    public function testUserCanUpdateTheirVehicle()
+    {
+        $user = User::factory()->create();
+        $vehicle = Vehicle::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->actingAs($user)->putJson('/api/v1/vehicles/' . $vehicle->id, [
+            'plate_number' => 'AAA123',
+        ]);
+
+        $response->assertStatus(202)
+            ->assertJsonStructure(['plate_number'])
+            ->assertJsonPath('plate_number', 'AAA123');
+
+        $this->assertDatabaseHas('vehicles', [
+            'plate_number' => 'AAA123',
+        ]);
+    }
+
+    public function testUserCanDeleteTheirVehicle()
+    {
+        $user = User::factory()->create();
+        $vehicle = Vehicle::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->actingAs($user)->deleteJson('/api/v1/vehicles/' . $vehicle->id);
+
+        $response->assertNoContent();
+
+        $this->assertDatabaseMissing('vehicles', [
+            'id' => $vehicle->id,
+            'deleted_at' => NULL
+        ])->assertDatabaseCount('vehicles', 1); // we have SoftDeletes, remember?
+    }
 }
