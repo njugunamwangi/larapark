0184b478c3c352c38986760707bb2b0e532e704f
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-09 20:54:59 +0300

    Zone management

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\V1\Auth\PasswordUpdateController;
 use App\Http\Controllers\Api\V1\Auth\ProfileController;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use App\Http\Controllers\Api\V1\VehicleController;
+use App\Http\Controllers\Api\V1\ZoneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -30,3 +31,4 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::post('auth/register', RegisterController::class);
 Route::post('auth/login', LoginController::class);
+Route::get('zones', [ZoneController::class, 'index']);
