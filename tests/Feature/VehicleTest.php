f0ca27f5bc20a177e7f417f70af69a92bdd68f0f
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-10 01:00:51 +0300

    Vehicle test

---
+++ tests/Feature/VehicleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Models\Vehicle;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class VehicleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserCanGetTheirOwnVehicles()
+    {
+        $john = User::factory()->create();
+        $vehicleForJohn = Vehicle::factory()->create([
+            'user_id' => $john->id
+        ]);
+
+        $adam = User::factory()->create();
+        $vehicleForAdam = Vehicle::factory()->create([
+            'user_id' => $adam->id
+        ]);
+
+        $response = $this->actingAs($john)->getJson('/api/v1/vehicles');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data'])
+            ->assertJsonCount(1, 'data')
+            ->assertJsonPath('data.0.plate_number', $vehicleForJohn->plate_number)
+            ->assertJsonMissing($vehicleForAdam->toArray());
+    }
+}
