0b42aa14c7feb91916c269d980158aba980107f0
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-09 19:54:15 +0300

    User login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('auth/register', RegisterController::class);
+Route::post('auth/login', LoginController::class);
