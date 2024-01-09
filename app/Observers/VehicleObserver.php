aca19cde3c35132daf08ab3b3518ff39e8dea5b5
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-09 20:41:23 +0300

    Vehicle management

---
+++ app/Observers/VehicleObserver.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Vehicle;
+
+class VehicleObserver
+{
+    public function creating(Vehicle $vehicle)
+    {
+        if (auth()->check()) {
+            $vehicle->user_id = auth()->id();
+        }
+    }
+    /**
+     * Handle the Vehicle "created" event.
+     */
+    public function created(Vehicle $vehicle): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Vehicle "updated" event.
+     */
+    public function updated(Vehicle $vehicle): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Vehicle "deleted" event.
+     */
+    public function deleted(Vehicle $vehicle): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Vehicle "restored" event.
+     */
+    public function restored(Vehicle $vehicle): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Vehicle "force deleted" event.
+     */
+    public function forceDeleted(Vehicle $vehicle): void
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\V1\Auth\LogoutController;
 use App\Http\Controllers\Api\V1\Auth\PasswordUpdateController;
 use App\Http\Controllers\Api\V1\Auth\ProfileController;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
+use App\Http\Controllers\Api\V1\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,6 +25,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('profile', [ProfileController::class, 'update']);
     Route::put('password', PasswordUpdateController::class);
     Route::post('auth/logout', LogoutController::class);
+    Route::apiResource('vehicles', VehicleController::class);
 });
 
 Route::post('auth/register', RegisterController::class);
