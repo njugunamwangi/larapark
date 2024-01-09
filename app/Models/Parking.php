f5167e2e54d816625164648ffe540c8df94fe3ec
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-09 19:32:59 +0300

    Parking

---
+++ app/Models/Parking.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Parking extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'start_time' => 'datetime',
+        'stop_time' => 'datetime',
+    ];
+}
