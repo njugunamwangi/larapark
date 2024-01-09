11455f7a698035520bb3d749a0d8ce02894c9eb8
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-09 20:44:49 +0300

    Exception handing

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,8 +24,12 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->is('api/v1/vehicles/*')) { // <- Add your condition here
+                return response()->json([
+                    'message' => 'Vehicle record not found.'
+                ], 404);
+            }
         });
     }
 }
